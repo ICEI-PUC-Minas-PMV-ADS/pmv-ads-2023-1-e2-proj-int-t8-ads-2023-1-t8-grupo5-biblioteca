7705e470fab40c1da25586e334fd3ae8c7140fd3
Silvestre Castro <silcs2027@gmail.com>
2023-06-11 21:07:46 -0300

    Livros

--- src/Biblioteca/Controllers/LivroController.cs
+++ src/Biblioteca/Controllers/LivroController.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Biblioteca.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Biblioteca.Models;
 
 namespace Biblioteca.Controllers
 {
     public class LivroController : Controller
     {
+
+        private readonly BancoContext _db;
+
+        public LivroController(BancoContext db)
+        {
+            _db = db;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<LivroModel> objList = _db.Livros;
+            return View(objList);
         }
     }
 }
